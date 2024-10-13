73869b0c7ed7ae03e8f511d119f06bd3be3c42ff
Jefferson Alves <61715554+jeffersonf-alves@users.noreply.github.com>
2024-10-13 11:57:11 -0300

    Configurações de rotas filhas

---
+++ pages/pacientes.vue
@@ -0,0 +1,36 @@
+<template>
+    <section>
+        <UtilsPageheader title="Pacientes" subtitle="Veja todas as informações dos seus pacientes" />
+        <UtilsTabs :tabs="tabs" @changedTab="changeChart($event)" />
+        <NuxtPage/>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tabs: [
+                {
+                    name: "Consultas realizadas",
+                    quantity: 25,
+                    default: true
+                },
+                {
+                    name: "Faturamento",
+                    quantity: null,
+                    default: false
+                }]       
+        }
+    },
+    methods: {
+        changeChart() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/pacientes/consultas.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg">
+        <h1>Ola, Pacientes</h1>
+    </div>
+</template>
+
+
+<style scoped>
+    .bg {
+        width: 500px;
+        height: 200px;
+        background-color: blue;
+    }
+</style>
